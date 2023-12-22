d4971604d6eef3bf7eadfd6838b47f3da5ec3b5d
jalal-82 <jalalkhanwork123@gmail.com>
2023-12-22 17:31:21 +1100

    Added more functions

--- Stockdata.py
+++ Stockdata.py
@@ -1,24 +1,10 @@
 import time
 import datetime
-import pandas as pd 
-
 import requests
+import csv
+import random
+import pandas as pd 
 import Keps
-
-
-def stock_data(ticker):
-    """
-    returns the open and close price of a stock
-    """
-    period1 = int(time.mktime(datetime.datetime(2023,11,1,23,59).timetuple()))
-    period2 = int(time.mktime(datetime.datetime(2023,11,30,23,59).timetuple()))
-    interval = "1wk" #1d,1m
-
-    query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
-
-    df = pd.read_csv(query_string)
-    result = print(df)
-    return result
 
 
 def get_stock_news(ticker, api_key):
@@ -48,13 +34,54 @@ def get_stock_news(ticker, api_key):
 
 
 api_key = Keps.api_key
-ticker = 'AMZN'
-news_items = get_stock_news(ticker, api_key)
-
-res = [[article['tickers'], article['date'], article['sentiment']] for article in news_items]
-print(res)
 
 
+stocks_csv = 'stocks_name.csv'
+
+def read_symbols(stocks_csv):
+    symbols = []
+
+    with open(stocks_csv, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)  # Using DictReader for easy access to columns by name
+
+        for row in reader:
+            symbols.append(row['Symbol'])  # Append the symbol from each row
+
+    # Select one random symbol from the list
+    random_symbol = random.choice(symbols) if symbols else None
+    return random_symbol
 
 
+def stock_data(ticker):
+    """
+    Returns the latest open and close price of a stock.
+    """
+    # Get today's date
+    today = datetime.datetime.now()
 
+    # Set period1 to the start of today and period2 to the end of today
+    period1 = int(time.mktime(today.replace(hour=0, minute=0, second=0, microsecond=0).timetuple()))
+    period2 = int(time.mktime(today.replace(hour=23, minute=59, second=59, microsecond=0).timetuple()))
+
+    interval = "1d"
+
+    # Construct the query string
+    query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
+
+    try:
+        df = pd.read_csv(query_string)
+        return df
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+print(stock_data(read_symbols(stocks_csv)))
+
+def main():
+    ticker = random_symbols
+    news_items = get_stock_news(ticker, api_key)
+
+    tickers_data = [[article['tickers']]for article in news_items]
+    sentimental_data = [[article['sentiment']] for article in news_items]
+    
+    return
